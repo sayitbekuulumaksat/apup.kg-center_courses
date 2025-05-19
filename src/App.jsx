01b601d5e7bc09f6c3ac4b32badf3f018289644a
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-05-20 00:20:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+    </>
+  );
+}
+
+export default App;
