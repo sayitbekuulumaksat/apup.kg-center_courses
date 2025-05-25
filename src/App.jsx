7cef46ac65f9bcf7f7a55c79fb9b7f23456f5c51
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-05-26 00:53:55 +0600

    add header and aboute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import About from "./components/About";
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <header>
         <Header />
       </header>
+      <main>
+        <About/>
+      </main>
     </>
   );
 }
